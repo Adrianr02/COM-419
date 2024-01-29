86a82b7cd88037036df010dfa85617ef01163fec
choakly <oakley1217@gmail.com>
2024-01-29 15:44:13 -0500

    Implemented Create Code

---
+++ Events/Create.js
@@ -0,0 +1,9 @@
+/// @description Insert description here
+// You can write your code in this editor
+/*
+x = 800;//Guy's x and y coords, can comment these out and manually out Guy in Room1 where we want him to spawn
+y = 100;
+*/
+Guy.speed = 4; //Guy's speed
+vsp = 0; //Vertical speed and gravity variables
+grv = 0.3;
